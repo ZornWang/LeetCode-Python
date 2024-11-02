b10d877b448158befd950847c514694596f0ab58
Zorn Wang <zornw@foxmail.com>
2024-11-02 18:41:31 +0800

    49. 字母异位词分组

---
+++ hot100/Solution49.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        pass
+
+
+if __name__ == "__main__":
+    strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+    print(Solution().groupAnagrams(strs))
