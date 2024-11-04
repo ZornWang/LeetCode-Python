b659356531d3bd824e03b6ca94409854d79ab5cd
Zorn Wang <zornw@foxmail.com>
2024-11-04 17:35:23 +0800

    49. 字母异位词分组

--- hot100/Solution49.py
+++ hot100/Solution49.py
@@ -1,9 +1,18 @@
 from typing import List
+import collections
 
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        pass
+        mp = collections.defaultdict(list)
+
+        for str in strs:
+            counts = [0] * 26
+            for ch in str:
+                counts[ord(ch) - ord("a")] += 1
+            mp[tuple(counts)].append(str)
+
+        return list(mp.values())
 
 
 if __name__ == "__main__":
