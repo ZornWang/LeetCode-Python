7468529d4ff1501e1cf9507d8300ad88022f582f
Zorn Wang <zornw@foxmail.com>
2024-11-06 17:26:01 +0800

    69. x 的平方根

---
+++ carl/array/binary_search/Solution69.py
@@ -0,0 +1,17 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        left, right, result = 0, x, -1
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid <= x:
+                result = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+        return result
+
+
+if __name__ == "__main__":
+    x = 0
+    s = Solution()
+    print(s.mySqrt(x))
