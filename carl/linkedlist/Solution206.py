b938891531948d2179ea9e38df24cd67ac245eb5
Zorn Wang <zornw@foxmail.com>
2024-11-17 16:19:42 +0800

    206. 反转链表

---
+++ carl/linkedlist/Solution206.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        cur = head
+        pre = None
+        while cur:
+            temp = cur.next
+            cur.next = pre
+            pre = cur
+            cur = temp
+        return pre
+
+
+if __name__ == "__main__":
+    for i in range(10, 0, -1):
+        print(i)
