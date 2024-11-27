5a9efc2b1e25e643a5237c3960aefb54d87b89ca
Zorn Wang <zornw@foxmail.com>
2024-11-27 17:25:32 +0800

    142. 环形链表 II

--- carl/linkedlist/Solution142.py
+++ carl/linkedlist/Solution142.py
@@ -9,12 +9,20 @@ class ListNode:
 
 
 class Solution:
-    # 3 -> 2 -> 0 -> 4 -> 2(cycle)
+    # 3 -> 2 -> 0 -> -4 -> 2(cycle)
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
         fast, slow = head, head
         while fast is not None and fast.next is not None:
+            # 1: fast(0) -> -4 -> 2 -> 0 -> -4 -> 2(cycle)
+            # 2: fast(2) -> 0 -> -4 -> 2(cycle)
+            # 3: fast(-4) -> 2 -> 0 -> -4 -> 2(cycle); fast == slow
             fast = fast.next.next
+            # 1: slow(2) -> 0
+            # 2: slow(0) -> -4
+            # 3: slow(-4) -> 2; slow == fast
             slow = slow.next
+            # fast(-4) -> 2 -> 0 -> -4 -> 2(cycle)
+            # head(3) -> 2 -> 0 -> -4 -> 2(cycle)
             if fast == slow:
                 p1 = head
                 p2 = fast
