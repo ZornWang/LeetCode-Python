2a58354c0ce95e0e0cfaf6f7f2b777d86c04a9dc
Zorn Wang <zornw@foxmail.com>
2024-12-06 21:59:05 +0800

    28. 找出字符串中第一个匹配项的下标

--- carl/string/Solution28.py
+++ carl/string/Solution28.py
@@ -1,14 +1,50 @@
+from typing import List
+
+
 class Solution:
-    # Normal Solution
+    # # Normal Solution
+    # def strStr(self, haystack: str, needle: str) -> int:
+    #     n = len(haystack)
+    #     m = len(needle)
+    #     for i in range(n - m + 1):
+    #         a = i
+    #         b = 0
+    #         while b < m and haystack[a] == needle[b]:
+    #             a += 1
+    #             b += 1
+    #         if b == m:
+    #             return i
+    #     return -1
+
+    def getNext(self, next: List[int], s: str) -> None:
+        j = 0
+        next[0] = 0
+        for i in range(1, len(s)):
+            while j > 0 and s[i] != s[j]:
+                j = next[j - 1]
+            if s[i] == s[j]:
+                j += 1
+            next[i] = j
+
     def strStr(self, haystack: str, needle: str) -> int:
-        n = len(haystack)
-        m = len(needle)
-        for i in range(n - m + 1):
-            a = i
-            b = 0
-            while b < m and haystack[a] == needle[b]:
-                a += 1
-                b += 1
-            if b == m:
-                return i
+        if len(needle) == 0:
+            return 0
+        next = [0] * len(needle)
+        self.getNext(next, needle)
+        j = 0
+        for i in range(len(haystack)):
+            while j > 0 and haystack[i] != needle[j]:
+                j = next[j - 1]
+            if haystack[i] == needle[j]:
+                j += 1
+            if j == len(needle):
+                return i - j + 1
         return -1
+
+
+if __name__ == "__main__":
+    s = "abaac"
+    solution = Solution()
+    next = [0] * len(s)
+    solution.getNext(next, s)
+    print(next)
